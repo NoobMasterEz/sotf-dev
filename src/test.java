cf0235054694a6d93dcdc5b77e562ff11d1cd060
ratchanonth <b13m123kiss@Gmail.com>
2019-11-25 16:12:15 +0700

    new

--- src/test.java
+++ src/test.java
@@ -1,30 +1,43 @@
-public class test{
-    int num=0;
-    test(int i){
-        num=i;
-    }
-    public static void main(String[] args) {
-        String[] f={"banan","apple","melon"};
-        String tel="053-211-122";
-        String[] sp =tel.split("-");
-        System.out.println(sp[0]);
-        test name=new test(10);
-        name.v1(f);
-        
-    }
-    public void v1(String [] fruits) {
-        for(int i =0 ;i<=fruits.length-1;i++)
-        {
-            System.out.println(fruits[i]);
-        }
-        /**
-         
-        
-         
-        for (String t : fruits) {
-            System.out.println(t);
-        }
-        */
-    }
-    
+Live Demo
+class ThreadDemo extends Thread {
+   private Thread t;
+   private String threadName;
+   
+   ThreadDemo( String name) {
+      threadName = name;
+      System.out.println("Creating " +  threadName );
+   }
+   
+   public void run() {
+      System.out.println("Running " +  threadName );
+      try {
+         for(int i = 4; i > 0; i--) {
+            System.out.println("Thread: " + threadName + ", " + i);
+            // Let the thread sleep for a while.
+            Thread.sleep(50);
+         }
+      } catch (InterruptedException e) {
+         System.out.println("Thread " +  threadName + " interrupted.");
+      }
+      System.out.println("Thread " +  threadName + " exiting.");
+   }
+   
+   public void start () {
+      System.out.println("Starting " +  threadName );
+      if (t == null) {
+         t = new Thread (this, threadName);
+         t.start ();
+      }
+   }
+}
+
+public class TestThread {
+
+   public static void main(String args[]) {
+      ThreadDemo T1 = new ThreadDemo( "Thread-1");
+      T1.start();
+      
+      ThreadDemo T2 = new ThreadDemo( "Thread-2");
+      T2.start();
+   }   
 }
